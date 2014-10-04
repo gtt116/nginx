ce8afc5bed9a03d4f6195f8b67b76a6416145bc5
gtt116 <gtt116@gmail.com>
2014-10-04 13:34:45 +0000

    Add run_dev.sh script
    The script file containes configure arguments, subsequence call make to compile
    nginx.

---
+++ run_dev.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./configure --prefix=/etc/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-client-body-temp-path=/var/lib/nginx/body --http-fastcgi-temp-path=/var/lib/nginx/fastcgi --http-log-path=/var/log/nginx/access.log --http-proxy-temp-path=/var/lib/nginx/proxy --http-scgi-temp-path=/var/lib/nginx/scgi --http-uwsgi-temp-path=/var/lib/nginx/uwsgi --lock-path=/var/lock/nginx.lock --pid-path=/var/run/nginx.pid  --sbin-path=/usr/sbin/nginx --add-module=../ngx_http_prelog_module
+
+make
